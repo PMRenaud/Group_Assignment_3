b5bfcceeb0a357c6d347872a36a719e62ed71f67
ChristosGiannikos98 <113847135+ChristosGiannikos98@users.noreply.github.com>
2022-11-15 23:01:58 +0000

    Add files via upload
    Some initial outlione of how i think the code should be.

---
+++ proj4_christos.R
@@ -0,0 +1,67 @@
+newt <- function(theta,func,grad,hess=NULL,...,tol=1e-8,fscale=1,
+                 maxit=100,max.half=20,eps=1e-6){
+  
+  f <- func(theta)
+  grad <- grad(theta)
+  if (!is.null(hess)){
+  hessian <- hess(theta)
+  }else{ 
+    hessian <- finite_differencing(f,theta,grad) 
+  }
+  if (!check_positive_definite(hessian)){
+    hessian <- positive_definite(hessian)
+    }
+  count <- 0
+  while(min(abs(grad)) > (tol * (abs(f) + fscale)) && count < maxit){
+    theta <- theta - chol2inv(hessian) %*% grad
+    f <- func(theta)
+    grad <- grad(theta)
+    if (!is.null(hess)){
+      hessian <- hess(theta)
+    }else{
+      hessian<- finite_differencing(f,theta,grad) 
+    }
+    hessian <- positive_definite(hessian)
+    count <- count + 1
+  }
+}
+
+finite_differencing <- function(f,theta,grad){
+  ## like pg 72 in notes
+  dim = length(theta)
+  Hfd <- matrix(0,dim,dim) ## finite diference Hessian
+  for (i in 1:dim) { ## Looping over parameters
+    th1 <- thetanew;
+    th1[i] <- th1[i] + eps        ## Increase th1[i] by eps 
+    grad1 <- grad(th1,...) 
+    H[i,] <- (grad1 - ftheta)/eps ## Approximate second derivatives
+    hessian <- H
+  }
+}
+
+
+check_positive_definite <- function(A){
+  result <- try(chol(A))
+  sf <- inherits(result,"matrix")
+  if (sf==TRUE){
+    return(TRUE)
+  }else{
+    return(FALSE)
+  }
+}
+positive_definite <- function(A){
+  if (check_positive_definite(A)==TRUE){
+    return(A)
+  }else{
+    l < 1e-06
+    temp < -A
+    A <- A +l*diag(1,dim(A)[1])
+    result <- try(chol(A))
+    sf <- inherits(result,"matrix")
+    while (check_positive_definite(A)==TRUE){
+      l=10*l
+      A <- A +l*diag(1,dim(A)[1])
+    }
+    return(A)
+  }
+}
