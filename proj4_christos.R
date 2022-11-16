2145b017082cf52f628a4300b963041d2f53b141
ChristosGiannikos98 <113847135+ChristosGiannikos98@users.noreply.github.com>
2022-11-16 14:44:19 +0000

    Add files via upload
    Some initial comments added.

--- proj4_christos.R
+++ proj4_christos.R
@@ -1,10 +1,32 @@
+## Statistical Programming, Practical 4
+## Github repo: https://github.com/
+## Christos Giannikos (s2436019), Elliot Leishman (s1808740), 
+## Patrick Renaud (s2462989)
+
+## The purpose of this practical is to construct an implementation of Newton's 
+## method for mininizing functions. This is an iterative method that constructs
+## a sequence of values (theta), starting from an initial guess theta(0) that 
+## converges towards a minimizer  by using second-order Taylor approximations 
+## of the objective function f around the constructed sequence values. The 
+## iteration stops when the method determines a value where all elements of the
+## gradient vector have an absolute value which is lower than a certain limit.
+
+## In order to implement Newton's method, we have constructed a function named
+## newt, which constructs the desired sequence. We have also created some other
+## functions which are called by newt. These are finite_differencing, 
+## check_positive_definite and positive_definite respectively.
+
 newt <- function(theta,func,grad,hess=NULL,...,tol=1e-8,fscale=1,
                  maxit=100,max.half=20,eps=1e-6){
+  ## 
   
+  ## iteration stops when the method determines a value where all elements of the
+  ## gradient vector have an absolute value which is lower than tol times the 
+  ## absolute value of the objective function plus fscale
   f <- func(theta)
   grad <- grad(theta)
   if (!is.null(hess)){
-  hessian <- hess(theta)
+    hessian <- hess(theta)
   }else{ 
     hessian <- finite_differencing(f,theta,grad) 
   }
@@ -23,12 +45,15 @@ newt <- function(theta,func,grad,hess=NULL,...,tol=1e-8,fscale=1,
       hessian<- finite_differencing(f,theta,grad) 
     }
     hessian <- positive_definite(hessian)
-    while(f>=f0){
+    count <- count + 1
+    halving_iterations <- 0
+    while(f>=f0 && halving_iterations <= max.half){
       f0 <- f
       theta <- theta - 0.5*chol2inv(hessian) %*% grad
       f <- func(theta)
       grad <- grad(theta)
-    count <- count + 1
+      halving_iterations <- halving_iterations + 1
+      count <- count + halving_iterations
   }
   }
 }
@@ -48,6 +73,13 @@ finite_differencing <- function(f,theta,grad){
 
 
 check_positive_definite <- function(A){
+  ## This function checks whether the input matrix A, which is given as an 
+  ## argument, is positive definite. When the input matrix corresponds to a 
+  ## Hessian matrix of a function f at some point theta, then this function 
+  ## actually returns whether or not the approximated quadratic of f estimated 
+  ## at theta, as used in the newt function, has a proper minimum.
+  ## Recall that positive definiteness is easily tested by seeing if a Cholesky 
+  ## decomposition is possible.
   result <- try(chol(A))
   sf <- inherits(result,"matrix")
   if (sf==TRUE){
@@ -57,6 +89,8 @@ check_positive_definite <- function(A){
   }
 }
 positive_definite <- function(A){
+  ## This function accepts as an argument a square matrix A and, if A is not 
+  ## a positive definite matrix, it perturbes it to be so. 
   if (check_positive_definite(A)==TRUE){
     return(A)
   }else{
